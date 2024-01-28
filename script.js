10798a8754593df32bf0669907fad2a4e9061e44
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2024-01-28 12:32:14 -0500

    Made the basic input form and title

---
+++ script.js
@@ -0,0 +1,7 @@
+const celcius = document.querySelector("#cel");
+const farenheit = document.querySelector("#far");
+
+function getData() {
+    celcius.innerHTML = "Hello World";
+    farenheit.innerHTML = "Hello World again!";
+}
