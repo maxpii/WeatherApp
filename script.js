1458b0c30687a34a9a9b97c44fbfa1abc16c8f55
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2024-02-06 21:26:48 -0500

    Code can now call my Weather API. Changed key for security reasons
    Gives information about Paris right now.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const celcius = document.querySelector("#cel");
 const farenheit = document.querySelector("#far");
-
+let key = "a35e785463724d3eaa820526240702";
 function getData() {
-    celcius.innerHTML = "Hello World";
-    farenheit.innerHTML = "Hello World again!";
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=Paris`)
+    .then(response => response.json())
+    .then(data => console.log(data));
 }
+getData();
