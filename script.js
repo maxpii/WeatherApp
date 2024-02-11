c483a12542e3e468fa7760c4007f7210fe0ef20c
Muhammad Raza <108022883+maxpii@users.noreply.github.com>
2024-02-11 00:18:05 -0500

    Now gets weather based off of location
    Need to style now

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const celcius = document.querySelector("#cel");
-const farenheit = document.querySelector("#far");
-let key = "a35e785463724d3eaa820526240702";
-function getData() {
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=Paris`)
-    .then(response => response.json())
-    .then(data => console.log(data));
+let celcius = document.querySelector("#cel");
+let farenheit = document.querySelector("#far");
+let form = document.querySelector("#textBox");
+let description = document.querySelector("#description");
+let image = document.querySelector("#image");
+let key = "4214162e5eab401eb5322634240702";
+function getData(){
+    let something = 0;
+fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${form.value}`)
+    .then(response => {
+        if(!response.ok) {
+            celcius.innerHTML = "City Not Found";
+            farenheit.innerHTML = "City Not Found";
+        }
+        else {
+            return response.json();
+        }
+    })
+    .then(data => {
+        console.log(data);
+        celcius.innerHTML = "Celcius: " + data.current.temp_c;
+        farenheit.innerHTML = "Farenheit: " + data.current.temp_f;
+        description.innerHTML = "Description " + data.current.condition.text;
+        image.src = data.current.condition.icon;
+    });
 }
-getData();
