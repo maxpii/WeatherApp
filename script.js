68b034a1a8f4c4b2143a83a9bf779a4506f45773
Muhammad Raza <hmrazarafique@gmail.com>
2024-05-17 21:21:20 -0400

    fixed styling

---
+++ script.js
@@ -0,0 +1,26 @@
+let celcius = document.querySelector("#cel");
+let farenheit = document.querySelector("#far");
+let form = document.querySelector("#textBox");
+let description = document.querySelector("#description");
+let image = document.querySelector("#image");
+let key = "4cf281303e344d838e705119241805";
+function getData(){
+    let something = 0;
+fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${form.value}`)
+    .then(response => {
+        if(!response.ok) {
+            celcius.innerHTML = "City Not Found";
+            farenheit.innerHTML = "City Not Found";
+        }
+        else {
+            return response.json();
+        }
+    })
+    .then(data => {
+        console.log(data);
+        celcius.innerHTML = "Celcius: " + data.current.temp_c;
+        farenheit.innerHTML = "Farenheit: " + data.current.temp_f;
+        description.innerHTML = "Description: " + data.current.condition.text;
+        image.src = data.current.condition.icon;
+    });
+}
